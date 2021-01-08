eaa8879eea63b47157b54926f419160571bfea1a
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2021-01-09 00:27:17 +0100

    websocket prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 const domain = 'https://pokern.herokuapp.com';
+const domain_ws = 'wss://pokern.herokuapp.com';
 
 export const environment = {
     production: true,
     poker_api: domain + '/api/poker',
-    socket_url: domain
+    socket_url: domain_ws
 };
