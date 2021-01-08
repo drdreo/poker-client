22116504c68149d89f2c43f3bdebb880e440c90d
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2021-01-09 00:04:50 +0100

    added prod frontend domain for CORS

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
-export const environment = {
-	production: true,
-	poker_api: 'https://pokern.herokuapp.com/api/poker',
-	socket_url: 'https://pokern.herokuapp.com:8988',
+const domain = 'https://pokern.herokuapp.com';
 
+export const environment = {
+    production: true,
+    poker_api: domain + '/api/poker',
+    socket_url: domain + ':8988'
 };
--- server/main.ts
+++ server/main.ts
@@ -3,6 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const whitelist = ['https://pokern.netlify.app'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
