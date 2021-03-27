9bbeddf10cf4563bb6cfd70a38124b9055a4cae2
DrDreo <code@andreas-hahn.at>
2021-03-27 14:08:23 +0100

    Fixed tests and winner message
    closes #44
    closes #28

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,28 +1,28 @@
-import { Winner } from '@shared/src';
-
+import { Winner, PotType } from '@shared/src';
 
 export function formatWinnersMessage(winners: Winner[]): string {
-    let message = '';
-    if (winners.length === 1) {
-        message = `${ winners[0].name } won the pot of ${ winners[0].amount }`;
-        if (winners[0].hand) {
+    const messages = [];
+    for (const winner of winners) {
+        let message = '';
+        message = `${ winner.name } won the ${ getPotTypeName(winner.potType) } pot of ${ winner.amount }`;
+        if (winner.hand) {
             message += ` with ${ winners[0].hand.descr }`;
         }
-    } else {
-        const winnerNames = winners.map(winner => winner.name);
-        const winnerPots = winners.map(winner => winner.amount);
-
-        const mainPot = winners.filter(winner => winner.potType === 'main');
-        if (mainPot.length > 1 && winners.every(winner => winner.potType === 'main')) {
-            console.log('Split pot detected!');
-            const splitPotWinnerNames = mainPot.map(winner => winner.name);
-            message = `${ splitPotWinnerNames.join(' & ') } split the pot for ${ mainPot[0].amount } each`;
-        } else {
-            message = `${ winnerNames.join(' & ') } won the pots of ${ winnerPots.join(' & ') }`;
-        }
+        messages.push(message);
     }
 
-    console.log(winners.map(winner => console.log(winner.hand.descr)));
+    console.log(winners.map(winner => winner.hand?.descr));
 
-    return message;
+    return messages.join('\n');
+}
+
+
+function getPotTypeName(potType: PotType): string {
+    switch (potType) {
+        case 'main':
+            return 'main';
+        case 'sidepot':
+        default:
+            return 'sidepot';
+    }
 }
